3bf75291a1ea4345702495af454406ac9b433d65
Jens Windmüller <jwindmueller@prodyna.com>
2024-02-26 17:40:39 +0100

    github actions and testreport

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,3 @@
-import './commands'
-import addContext from 'mochawesome/addContext'
 
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
@@ -13,13 +11,6 @@ module.exports = defineConfig({
       "configFile": "reporter-config.json"
     },
     setupNodeEvents(on, config) {
-      on("test:after:run", (test, runnable) => {
-        let videoName = Cypress.spec.name;
-        videoName = videoName.replace('/.js.*', '.js');
-        const videoUrl = 'videos/' + videoName + '.mp4';
-
-        addContext({ test }, videoUrl);
-      });
       // implement node event listeners here
     }
   }
