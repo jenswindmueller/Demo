ae2cbecf0ea40175b0840429d446222de04d24c1
Jens Windmüller <jwindmueller@prodyna.com>
2024-02-26 16:40:32 +0100

    github actions and testreport

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("cypress");
-
-
 module.exports = defineConfig({
   e2e: {
     // baseUrl: "https://demoauthor.magnolia-cms.com",
     viewportWidth: 1280,
     viewportHeight: 1080,
+    video: true,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      "configFile": "reporter-config.json"
+    },
     setupNodeEvents(on, config) {
+      on("test:after:run", (test, runnable) => {
+        let videoName = Cypress.spec.name;
+        videoName = videoName.replace('/.js.*', '.js');
+        const videoUrl = 'videos/' + videoName + '.mp4';
+
+        addContext({ test }, videoUrl);
+      });
       // implement node event listeners here
     }
   }
